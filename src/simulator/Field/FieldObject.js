85c32a8a3dea27569475a91f882301219c16a97b
karbuncle <karbuncle@puyo.tw>
2019-01-12 18:14:10 -0800

    改善FieldObject描述。

--- src/simulator/Field/FieldObject.js
+++ src/simulator/Field/FieldObject.js
@@ -4,10 +4,24 @@ export default FieldObject;
 Object.defineProperty(FieldObject, 'Properties', {
   value : class {
     constructor({
-      /** Function that, given positional, determines what this object should become when cleared. */
+      /**
+       * Function that, given positional, determines what this object should 
+       * become when cleared. Default is that it becomes itself; logically it
+       * means the object won't be cleared.
+       */
       // eslint-disable-next-line
       cleared         = (positional) => this,
-      /** Function that, given positional, determines what this object should become when an adjacent object is cleared. */
+      /** 
+       * Function that, given positional, determines what this object should 
+       * become when an adjacent object is cleared. Default is that it becomes
+       * itself; logically it means the object won't be affected by adjacent
+       * clearance.
+       *
+       * Note that it is often a bad idea to have clearable objects be affected
+       * by adjacent clearance, as the final result will have to depend on
+       * clearance order. This behavior is currently undefined as we don't
+       * have a use case for it yet.
+       */
       // eslint-disable-next-line
       adjacentCleared = (positional) => this,
       /** Determines whether this object should be immune to gravity. */
